fedf955acd2e9cc0359d9972b5384077b0fdf2cb
mortada335 <a80923904@gmail.com>
2024-08-07 12:39:41 +0300

    edited vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,7 @@ module.exports = defineConfig({
       treeShake: true
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
     }
-  }
+  }, publicPath: process.env.NODE_ENV === 'production'
+    ? '/academy/'
+    : '/'
 })
